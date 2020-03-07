6b2cfa2111566d20e60c8980ac98a8c3a7fe25ce
howtoautomateinth <mart@howtoautomate.in.th>
2020-03-07 20:35:09 +0700

    Update script for setup credential

--- scripts/darwin/export-token-slack.sh
+++ scripts/darwin/export-token-slack.sh
@@ -1,1 +1,2 @@
 export SLACK_BOT_TOKEN='your bot user access token here'
+export SLACK_SIGNING_SECRET='your signing secret here'
--- scripts/win32/export-token-slack.ps1
+++ scripts/win32/export-token-slack.ps1
@@ -1,2 +1,3 @@
- $env:SLACK_BOT_TOKEN='your bot user access token here'
+ $env:SLACK_BOT_TOKEN="your bot user access token here"
+ $env:SLACK_SIGNING_SECRET="your signing secret here"
  dir env:
